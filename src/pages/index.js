7373ebe8bb72b43de9e5f97ff05061b7bc80e80c
Benedicte Raae <raae@bgraphic.no>
2020-11-29 11:06:29 +0100

    Add theme-ui for styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,25 @@
 import React from "react"
+import { Button, Flex, Box } from "theme-ui"
 
 export default function Home() {
   return (
-    <button style={{ margin: "3em" }} onClick={() => alert("Play sound")}>
-      Bottle placeholder
-    </button>
+    <Flex
+      bg=""
+      sx={{
+        height: "100vh",
+        width: "100vw",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box>
+        <Button
+          onClick={() => alert("Play sound")}
+          sx={{ height: "80vh", width: "clamp(30px, 30vw, 600px)" }}
+        >
+          Bottle placeholder
+        </Button>
+      </Box>
+    </Flex>
   )
 }
