8d73b3781e2aec96ea513906c8da0b6ee572e715
Ola Vea <ola@olavea.com>
2020-12-09 09:47:35 +0100

    added a laugh from Freesound #3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react"
 import { Button, Flex, Box } from "theme-ui"
+
+
 import useSound from 'use-sound'
 
 // record Lillian (5.5 🦄) and Liv (7) and add laugh1 in the sounds folder
-import laugh1 from '../../sounds/g_is_for_grr.mp3';
+import laugh1 from '../../sounds/534709__artymarce__childlaugh.mp3';
 
 export default function Home() {
-  const [play] = useSound(laugh1);
+  const [play, {stop}] = useSound(laugh1);
   return (
     <Flex
       bg=""
@@ -20,6 +22,8 @@ export default function Home() {
       <Box>
         <Button
           onClick={play}
+          // What should the user do to stop laughter?
+          // Frown? Say "STOP laughing!"
           sx={{ height: "80vh", width: "clamp(30px, 30vw, 600px)" }}
         >
           Bottle placeholder
