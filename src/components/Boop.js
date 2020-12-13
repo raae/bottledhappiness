df37f2c715ea9c415b315b4877c8b103821beb09
Benedicte Raae <raae@bgraphic.no>
2020-12-13 11:07:23 +0100

    Added a tiny useBoop animation to the bottle.

---
+++ src/components/Boop.js
@@ -0,0 +1,16 @@
+// components/Boop.jsx
+import React from "react"
+import { animated } from "react-spring"
+import useBoop from "../hooks/useBoop"
+
+const Boop = ({ children, ...boopConfig }) => {
+  const [style, trigger] = useBoop(boopConfig)
+
+  return (
+    <animated.span onClick={trigger} style={style}>
+      {children}
+    </animated.span>
+  )
+}
+
+export default Boop
