1ed1c2aa7e4c0e8db9126525ae77bd710fd55126
Benedicte Raae <raae@bgraphic.no>
2020-12-20 11:51:07 +0100

    Try using Spring render props.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
 /** @jsx jsx */
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
-import { Button, Flex, Box, jsx } from "theme-ui"
+import { Flex, Box, jsx } from "theme-ui"
+import { Spring, config } from "react-spring/renderprops"
 import useSound from "use-sound"
 import laugh1 from "../../laughs/534709__artymarce__childlaugh.mp3"
 import IllustrationSVG from "../assets/illustration.svg"
-import Boop from "../components/Boop"
 
 export default function Home() {
-  const [play, { stop }] = useSound(laugh1)
+  const [play] = useSound(laugh1)
+  const [reverse, setReverse] = useState(false)
+
   return (
     <Flex
       bg=""
@@ -20,14 +22,45 @@ export default function Home() {
         flexDirection: "column",
       }}
     >
-      <Box>
-        <Boop y={5}>
+      <Spring
+        from={{
+          transform: `translate(0px, 0px) rotate(0deg) scale(1)`,
+        }}
+        to={{
+          transform: `translate(${0}px, ${5}px) rotate(${0}deg) scale(${1})`,
+        }}
+        reset={true}
+        reverse={reverse}
+        config={config.molasses}
+        onRest={() => {
+          setReverse(current => !current)
+        }}
+      >
+        {props1 => (
           <IllustrationSVG
             onClick={play}
-            sx={{ height: "70vh", width: "clamp(30px, 30vw, 600px)" }}
+            sx={{
+              height: "90vh",
+              width: "clamp(30px, 30vw, 600px)",
+              path: {
+                ":nth-child(3n)": {
+                  fill: "red",
+                },
+                ":nth-child(4n)": {
+                  fill: "blue",
+                  ...props1,
+                },
+                ":nth-child(5n)": {
+                  fill: "pink",
+                },
+                ":nth-child(8n)": {
+                  fill: "yellow",
+                },
+              },
+            }}
           />
-        </Boop>
-      </Box>
+        )}
+      </Spring>
 
       <Link sx={{ fontFamily: "body", p: 3, display: "block" }} to="about">
         About
