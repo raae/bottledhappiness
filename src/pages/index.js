30c59988fc3282c7a634603de8b84aa9eaeeee6a
Ola Vea <ola@olavea.com>
2020-12-28 09:23:55 +0100

    added         <svg xmlns="http://w3.org/2000/svg" viewBox="0 0 100 100"> <text y=".9em" font-size="90">💩</text> </svg> #29

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,15 @@ export default function Home() {
 
   return (
     <>
-      <Helmet title="🍾🤣🍾" />
+      <Helmet>
+        <title>🍾🤣🍾</title>
+        <link rel="icon"
+        href="data:image/svg+xml,
+        <svg xmlns=%22http://w3.org/2000/svg%22 viewBox=%220 0 100 100%22>
+        <text y=%22.9em%22 font-size=%2290%22>💩</text>
+        </svg>" />
+
+      </Helmet>
 
       <Flex
         bg=""
