15bac7ad5040ad72b61a043f59bc17798fd1a4eb
Ola Vea <ola@olavea.com>
2020-12-29 10:04:16 +0100

    new favicon from emoji #29

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,11 +26,7 @@ export default function Home() {
     <>
       <Helmet>
         <title>🍾🤣🍾</title>
-        <link rel="icon"
-        href="data:image/svg+xml,
-        <svg xmlns=%22http://w3.org/2000/svg%22 viewBox=%220 0 100 100%22>
-        <text y=%22.9em%22 font-size=%2290%22>💩</text>
-        </svg>" />
+        <link rel="icon" href="data:image/svg+xml, <svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>😸</text></svg>" />
 
       </Helmet>
 
