5b49288f123b8b1c420807d60706b9e11adf24f6
Ola Vea <ola@olavea.com>
2020-12-30 10:57:10 +0100

    🤣 2  {MarkdownRemark.parent__(File)__name}.js #29

--- src/pages/{MarkdownRemark.parent__(File)__name}.js
+++ src/pages/{MarkdownRemark.parent__(File)__name}.js
@@ -8,7 +8,10 @@ const MarkdownTemplate = ({ data }) => {
   const { theme } = useThemeUI()
   return (
     <>
-      <Helmet title="🍾🤣🍾 - About" />
+      <Helmet>
+        <title>🍾🤣🍾 - About </title>
+        <link rel="icon" href="data:image/svg+xml, <svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🤣</text></svg>" />
+      </Helmet>
 
       <main
         sx={{
