11c35f13b8240cc7e140d139f10a8e7fa2a1c6a5
Benedicte Raae <raae@bgraphic.no>
2020-12-31 12:31:14 +0100

    Move content into gatsby config and source it with a static query

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -1,17 +1,33 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
 
 const Meta = () => {
+  const { site } = useStaticQuery(query)
+  const { title, description, favEmojii } = site.siteMetadata
+  const favIcon = `data:image/svg+xml, <svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>
+    ${favEmojii}
+    </text></svg>`
+
   return (
     <Helmet>
-      <title>Bottled Happiness 🍾🤣🍾</title>
-      <meta name="description" content="A Bottle of our Daughter's Laughter" />
-      <link
-        rel="icon"
-        href="data:image/svg+xml, <svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍾</text></svg>"
-      />
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <link rel="icon" href={favIcon} />
     </Helmet>
   )
 }
 
 export default Meta
+
+const query = graphql`
+  query Meta {
+    site {
+      siteMetadata {
+        title
+        description
+        favEmojii
+      }
+    }
+  }
+`
