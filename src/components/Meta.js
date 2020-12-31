ab391d179e33eddbe7be01436d2488b60b5de54a
Benedicte Raae <raae@bgraphic.no>
2020-12-31 13:20:37 +0100

    Pleasing fb and twitter

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -11,6 +11,7 @@ const Meta = () => {
     siteImage,
     siteUrl,
   } = site.siteMetadata
+
   const socialImage = `${siteUrl}${siteImage}`
   const favIcon = `data:image/svg+xml, <svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>
     ${favEmojii}
@@ -22,6 +23,13 @@ const Meta = () => {
       <meta name="description" content={description} />
       <link rel="icon" href={favIcon} />
       <meta name="image" content={socialImage} />
+
+      <meta property="og:title" content={title} />
+      <meta property="og:image" content={socialImage} />
+
+      <meta property="twitter:title" content={title} />
+      <meta name="twitter:image" content={socialImage} />
+      <meta name="twitter:card" content="summary_large_image" />
     </Helmet>
   )
 }
