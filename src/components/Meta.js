993b912b5d114ff0edf6defdfd2b0652de21dec5
Benedicte Raae <raae@bgraphic.no>
2020-12-31 12:14:01 +0100

    Refactor Helmet code into a Meta component

---
+++ src/components/Meta.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+
+const Meta = () => {
+  return (
+    <Helmet>
+      <title>Bottled Happiness 🍾🤣🍾</title>
+      <meta name="description" content="A Bottle of our Daughter's Laughter" />
+      <link
+        rel="icon"
+        href="data:image/svg+xml, <svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍾</text></svg>"
+      />
+    </Helmet>
+  )
+}
+
+export default Meta
